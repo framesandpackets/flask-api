68e45f13b582ba0b0dfb10346a0e7ae22879722f
FramesandPackets <michaelhanna272@hotmail.co.uk>
2020-11-16 21:24:11 +0000

    adding url variables and cleaning up code

--- app.py
+++ app.py
@@ -4,25 +4,17 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
-@app.route('/api')
-def super_simple():
-    return jsonify(message='Hello from the FramesandPackets API'), 200
-
-
-@app.route('/not_found')
-def not_found():
-   return jsonify(message='Data requested not found :('), 404
-
-
-@app.route('/parameters')
-def parameters():
-    name = request.args.get('name')
-    age = int(request.args.get('age'))
+@app.route('/api/<string:name>/<int:age>')
+def api(name: str, age: int):
     if age < 18:
         return jsonify(message="Sorry " + name + ", you are not old enough."), 401
     else:
         return jsonify(message="Hello!, " + name + "! you are old enough. "), 200
+    
 
+@app.route('/not_found')
+def not_found():
+   return jsonify(message='Data requested not found :('), 404
 
 
 if __name__ == '__main__':
