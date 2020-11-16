0258b23abc253a792448fb171eb1ce951c68ffcb
FramesandPackets <michaelhanna272@hotmail.co.uk>
2020-11-16 14:57:58 +0000

    explicit values added to status codes for 200 ok & 404 not_found

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route('/api')
 def super_simple():
-    return jsonify(message='Hello from the FramesandPackets API')
+    return jsonify(message='Hello from the FramesandPackets API'), 200
 
 @app.route('/not_found')
 def not_found():
-   return jsonify(message='Data requested not found :(')
+   return jsonify(message='Data requested not found :('), 404
 
 if __name__ == '__main__':
     app.run()
