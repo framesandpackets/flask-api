e607dd85023c8dd36a4ea36f2e5d748f86735a1d
FramesandPackets <michaelhanna272@hotmail.co.uk>
2020-11-16 16:40:17 +0000

    adding request module and added stable parameter check

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
@@ -8,9 +8,22 @@ app = Flask(__name__)
 def super_simple():
     return jsonify(message='Hello from the FramesandPackets API'), 200
 
+
 @app.route('/not_found')
 def not_found():
    return jsonify(message='Data requested not found :('), 404
 
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(message="Hello!, " + name + "! you are old enough. "), 200
+
+
+
 if __name__ == '__main__':
-    app.run()
+   app.run()
