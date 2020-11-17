97bb728c8cb601ee3620b9c6bb4f14f7311d8da2
FramesandPackets <michaelhanna272@hotmail.co.uk>
2020-11-17 10:42:11 +0000

    SQLAlchemy packages/modules added to app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+import os
 
 
 app = Flask(__name__)
@@ -10,7 +13,7 @@ def api(name: str, age: int):
         return jsonify(message="Sorry " + name + ", you are not old enough."), 401
     else:
         return jsonify(message="Hello!, " + name + "! you are old enough. "), 200
-    
+
 
 @app.route('/not_found')
 def not_found():
