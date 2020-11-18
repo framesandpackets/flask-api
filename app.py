029f0e9f9ae5de438e5ecf7b2918238cfe075bae
FramesandPackets <michaelhanna272@hotmail.co.uk>
2020-11-18 11:00:48 +0000

    mysql database create, seed and delete all running and stable. fake data added to test database

--- app.py
+++ app.py
@@ -6,7 +6,58 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'api.db)')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'api.db')
+
+
+db = SQLAlchemy(app)
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created!')
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Database dropped!')
+
+@app.cli.command('db_seed')
+def db_seed():
+    mercury = Planet(planet_name='Mercury',
+                     planet_type='Class D',
+                     home_star='Sol',
+                     mass=2.258e23,
+                     radius=1516,
+                     distance=35.98e6)
+
+    venus = Planet(planet_name='Venus',
+                         planet_type='Class K',
+                         home_star='Sol',
+                         mass=4.867e24,
+                         radius=3760,
+                         distance=67.24e6)
+
+    earth = Planet(planet_name='Earth',
+                     planet_type='Class M',
+                     home_star='Sol',
+                     mass=5.972e24,
+                     radius=3959,
+                     distance=92.96e6)
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+    test_user = User(first_name='test',
+                     last_name='user',
+                     email='test@test.com',
+                     password='password')
+
+
+    db.session.add(test_user)
+    db.session.commit()
+    print('Database seeded!')
+
 
 @app.route('/api/<string:name>/<int:age>')
 def api(name: str, age: int):
@@ -20,6 +71,24 @@ def api(name: str, age: int):
 def not_found():
    return jsonify(message='Data requested not found :('), 404
 
+# database models
+class User(db.Model):
+    ___tablename___ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
 
 if __name__ == '__main__':
    app.run()
